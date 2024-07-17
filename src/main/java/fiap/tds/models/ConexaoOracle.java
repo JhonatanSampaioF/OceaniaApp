6c599b5b909390262fe98f559917a6ad71ea3e06
Jhonatan Sampaio <148507574+JhonatanSampaioF@users.noreply.github.com>
2024-07-17 15:14:22 -0300

    Update ConexaoOracle.java

--- src/main/java/fiap/tds/models/ConexaoOracle.java
+++ src/main/java/fiap/tds/models/ConexaoOracle.java
@@ -1,7 +1,7 @@
 package fiap.tds.models;
 
 public class ConexaoOracle {
-    public static final String URL_CONNECTION = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+    public static final String URL_CONNECTION = "";
     public static final String USER = "RM553791";
-    public static final String PASSWORD = "180298";
+    public static final String PASSWORD = "";
 }
